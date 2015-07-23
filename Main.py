cee17c2edfed6f7d1752c7b2ec7ebe777cbb80ad
Shadow139 <cloud99_9@hotmail.com>
2015-07-23 16:19:44 +0200

    bird up and down movement

--- Main.py
+++ Main.py
@@ -21,6 +21,8 @@ img = pygame.image.load('Assets/bird.png')
 bird_x = 150
 bird_y = 200
 
+bird_y_move = 5
+
 
 game_over = False
 
@@ -28,6 +30,15 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+
+        if event.type == pygame.KEYDOWN :
+            if event.key == pygame.K_UP:
+                bird_y_move = -5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                bird_y_move = 5
+
+    bird_y += bird_y_move
 
     surface.fill(clr_black)
     bird(bird_x,bird_y,img)
