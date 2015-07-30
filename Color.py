8f43f12e3179203059aea7368027ec31201769da
Shadow139 <cloud99_9@hotmail.com>
2015-07-30 13:03:24 +0200

    format back

--- Color.py
+++ Color.py
@@ -3,24 +3,24 @@ __author__ = 'Wiktor'
 A Class that represents different colors for easy acess.
 """
 
-#Color_Name         R       G       B
-clr_black       = ( 0,      0,      0)
-clr_darkgrey    = ( 105,    105,   105)
-clr_grey        = ( 128,    128,   128)
-clr_lightGrey   = ( 190,    190,   190)
-clr_white       = ( 255,    255,   255)
+# Color_Name         R       G       B
+clr_black = (0, 0, 0)
+clr_darkgrey = (105, 105, 105)
+clr_grey = (128, 128, 128)
+clr_lightGrey = (190, 190, 190)
+clr_white = (255, 255, 255)
 
-clr_red         = ( 255,    0,      0)
-clr_green       = ( 0,      255,    0)
-clr_blue        = ( 0,      0,      255)
+clr_red = (255, 0, 0)
+clr_green = (0, 255, 0)
+clr_blue = (0, 0, 255)
 
-clr_maroon      = ( 128,    0,      0)
-clr_lime        = ( 0,      128,    0)
-clr_navyblue    = ( 0,      0,      128)
+clr_maroon = (128, 0, 0)
+clr_lime = (0, 128, 0)
+clr_navyblue = (0, 0, 128)
 
-clr_yellow      = ( 255,    255,    0)
-clr_cyan        = ( 0,      255,    255)
-clr_magenta     = ( 255,    0,      255)
+clr_yellow = (255, 255, 0)
+clr_cyan = (0, 255, 255)
+clr_magenta = (255, 0, 255)
 
-clr_skyblue     = ( 135,    206,    250)
-clr_orange      = ( 255,    128,    0)
+clr_skyblue = (135, 206, 250)
+clr_orange = (255, 128, 0)
